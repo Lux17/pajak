480a27561721d5465e0c88a638dc1ca7798f12b8
luckysaputra17 <damnlux17@gmail.com>
2022-12-08 23:58:47 +0700

    bg

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if(is_numeric($angka)){
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="assets/style.css" >
     <title>Hitung Pajak Penghasilan</title>
   </head>
   <body>
